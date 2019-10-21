78b9e7171de8b96e5dcdb9ebea9c8832469162c0
Reinhardt Novak <reinenovak17@hotmail.com>
2019-10-21 12:09:18 +0200

    yay it works

---
+++ MyFirstConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MyFirstConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("vemfanärsomdu");
+
+        }
+    }
+}
